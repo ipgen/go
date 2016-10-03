94848a8c3620cd8b197bfb9a8a1dee319db3f97d
rushmorem <rushmore@webenchanter.com>
2016-10-04 00:20:37 +0200

    Test V4In6 addresses

--- ipgen_test.go
+++ ipgen_test.go
@@ -11,3 +11,13 @@ func TestIPIsValid(t *testing.T) {
 		t.Fatal("Was expecting an IP address but none was produced")
 	}
 }
+
+func TestV4In6(t *testing.T) {
+	ip, err := IP("c0a010fb-2632-40cb-a105-90297cba567a", "::10.0.0.0/8")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ip.String() != "c9:31ec:e663:766c:2e4:826e:2391:6a3c" {
+		t.Fatalf("Expected: %s Got: %s\n", "c9:31ec:e663:766c:2e4:826e:2391:6a3c", ip)
+	}
+}
